5103c9e2734fc4c24937522c60c9be082afa2beb
RaulIQ <raulalimbekov@gmail.com>
2023-02-10 12:17:51 +0300

    update

--- ur.py
+++ ur.py
@@ -4,7 +4,11 @@ c = float(input())
 
 d = b**2 - 4 * a * c
 
-x1 = (-b - d**(0.5)) / 2 * a
-x2 = (-b + d**(0.5)) / 2 * a
-
-print(x1, x2)
+if d == 0:
+    print(-b / 2a)
+elif d > 0:
+    x1 = (-b - d**(0.5)) / 2 * a
+    x2 = (-b + d**(0.5)) / 2 * a
+    print(x1, x2)
+else:
+    print('Нет корней')
